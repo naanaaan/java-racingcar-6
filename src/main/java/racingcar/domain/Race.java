631cd2b8c6dee427f2a4f58677df92977a24e324
Seon Jeonggwan <sdfjikl8787@gamil.com>
2025-06-25 09:12:10 +0900

    feat(domain): 최종 우승자를 구하는 기능 구현

---
+++ src/main/java/racingcar/domain/Race.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Race {
+    private final List<Car> cars;
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moveAll() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public List<Car> findWinners() {
+        return cars.stream()
+                .filter(this::hasMaxPosition)
+                .toList();
+    }
+
+    private boolean hasMaxPosition(Car car) {
+        return car.getPosition() == findMaxPosition();
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .getAsInt();
+    }
+}
