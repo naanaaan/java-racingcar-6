eb65441e67e32e25afa310c62e7f7ea3080641cf
Seon Jeonggwan <sdfjikl8787@gamil.com>
2025-06-26 11:34:04 +0900

    test(validation): 시도 횟수 정상 흐름 테스트 코드 추가

--- src/test/java/racingcar/validate/TryCountValidatorTest.java
+++ src/test/java/racingcar/validate/TryCountValidatorTest.java
@@ -1,5 +1,6 @@
 package racingcar.validate;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,11 +8,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TryCountValidatorTest {
-    @DisplayName("시도 횟수가 숫자가 아닌 경우 IllegalArgumentException  발생한다.")
+    @DisplayName("시도 횟수가 숫자가 아닌 경우 IllegalArgumentException 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = { "?", " ", "a" })
     void trowsIllegalArgumentExceptionIfNotNummeric(String tryCount) {
-
         assertThatThrownBy(() -> TryCountValidator.validatePositiveNumeric(tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.TRYCOUnT_NOT_NUMERIC.getMessage());
@@ -21,9 +21,16 @@ public class TryCountValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = { "0", "-1", "-3" })
     void throwsIllegalArgumentExceptionIfNotPositiveNumeric(String tryCount) {
-
         assertThatThrownBy(() -> TryCountValidator.validatePositiveNumeric(tryCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.TRYCOUNT_NOT_POSITIVE.getMessage());
+    }
+
+    @DisplayName("옳바른 시도 횟수 입력 시, 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = { "1", "5", "10" })
+    void notThrowExceptionIfValidTryCount(String carName) {
+        assertThatCode(() -> TryCountValidator.validatePositiveNumeric(carName))
+                .doesNotThrowAnyException();
     }
 }
