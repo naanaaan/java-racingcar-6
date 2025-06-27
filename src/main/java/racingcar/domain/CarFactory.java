dadfd40edc2e80a4013c910392125f359f81fbde
Seon Jeonggwan <sdfjikl8787@gamil.com>
2025-06-27 14:52:49 +0900

    refactor(domain): CarFactory 클래스 private 생성자 추가

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarFactory {
+    private CarFactory() {};
+
     public static List<Car> createCars(List<String> carNames, MoveStrategy strategy) {
         return carNames.stream()
             .map(name -> new Car(name, strategy))
