492519e6646430b32f909104de7faa1c8ae8fa13
Seon Jeonggwan <sdfjikl8787@gamil.com>
2025-06-27 09:49:51 +0900

    feat(view): 최종 우승자를 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,13 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import racingcar.domain.CarDto;
 
 public class OutputView {
     private static final String POSITION_INDICATOR = "-";
+    private static final String WINNER_DELIMITER = ", ";
 
     public static void printInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -32,9 +34,14 @@ public class OutputView {
     private static String convertPositionToDash(int position) {
         return POSITION_INDICATOR.repeat(position);
     }
+
+    public static void printWinners(List<CarDto> winners) {
+        System.out.printf("최종 우승자 : %s", formatWinnerNames(winners));
+    }
+
+    private static String formatWinnerNames(List<CarDto> winners) {
+        return winners.stream()
+                .map(CarDto::getName)
+                .collect(Collectors.joining(WINNER_DELIMITER));
+    }
 }
-/*
- * 실행 결과 pobi : - woni : jun : -
- * 
- * pobi : -- woni : - jun : --
- */
