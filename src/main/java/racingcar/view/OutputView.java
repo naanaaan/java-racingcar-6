a81b5fc8e61ad5c8032a24946deb33936538434a
Seon Jeonggwan <sdfjikl8787@gamil.com>
2025-06-27 14:48:53 +0900

    refactor(view): OutputView private 생성자 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -9,6 +9,8 @@ public class OutputView {
     private static final String POSITION_INDICATOR = "-";
     private static final String WINNER_DELIMITER = ", ";
 
+    private OutputView() {};
+
     public static void printInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
